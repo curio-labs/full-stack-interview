619caf9f139467e4878115c757d7b4548f8bceca
Ivo <ivosequeros@me.com>
2022-07-28 10:52:56 +0100

    Adjustments
    Co-authored-by: Daniel Padmore <danielpadmore@users.noreply.github.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,20 @@
-import { RequestHandler } from "micro";
-import handle from "./handler";
-import router from "./router";
+import { send, json } from "micro";
+import { add, getAll, getSuggestion, remove } from "./db";
 
-export default ((req, res) => handle(req, res)(router)) as RequestHandler;
+const handler = async (req: any, res: any) => {
+	if (req.url === "/get-restaurants") {
+		send(res, 200, getAll());
+	} else if (req.url === "/get-suggested-restaurant") {
+		send(res, 201, JSON.stringify(getSuggestion()));
+	} else if (req.url === "/add-restaurant") {
+		const body = (await json(req)) as any;
+		send(res, 201, add(body.restaurant));
+	} else if (req.url === "/remove-restaurant") {
+		const body = (await json(req)) as any;
+		send(res, 201, remove(body.restaurant));
+	} else {
+		send(res, 404, JSON.stringify("not-found"));
+	}
+};
+
+export default handler;
